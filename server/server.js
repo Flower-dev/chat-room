931ef49572bf91b3fb1d8585eadc1647ca225a7a
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2023-01-01 11:58:39 +0100

    refacto server.js

--- server/server.js
+++ server/server.js
@@ -3,39 +3,38 @@ const app = express();
 const PORT = 8080;
 const http = require('http').Server(app);
 const cors = require('cors');
-// à changer avec ws
-const socketIO = require('socket.io')(http, { 
+
+const ws = require('socket.io')(http, { 
 	cors: {
-	  origin: "http://localhost:3000"
+		origin: "http://localhost:3000"
 	}
 });
 
 app.use(cors());
+
 let users = [];
 
-
-
-socketIO.on('connection', (socket) => {
+ws.on('connection', (socket) => {
     console.log(`⚡: ${socket.id} user just connected!`)  
     // sends the message to all the users on the server
     socket.on("message", data => {
-      socketIO.emit("messageResponse", data)
+    	ws.emit("messageResponse", data)
     })
 
     socket.on("typing", data => (
-      socket.broadcast.emit("typingResponse", data)
+    	socket.broadcast.emit("typingResponse", data)
     ))
 
     socket.on("newUser", data => {
-      users.push(data)
-      socketIO.emit("newUserResponse", users)
+    	users.push(data)
+    	ws.emit("newUserResponse", users)
     })
  
     socket.on('disconnect', () => {
-      console.log('🔥: A user disconnected');
-      users = users.filter(user => user.socketId !== socket.id)
-      socketIO.emit("newUserResponse", users)
-      socket.disconnect()
+    	console.log('🔥: A user disconnected');
+    	users = users.filter(user => user.socketId !== socket.id)
+    	ws.emit("newUserResponse", users)
+    	socket.disconnect()
     });
 });
 
