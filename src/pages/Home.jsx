6674eb5d5e046dbba6f90d85abeb9cdf2a328a4f
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2023-01-02 21:52:08 +0100

    refonte css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 // components
 import Button from "../components/atoms/Button";
 // assets
-import Img from "../assets/chatImg.svg";
+import Img from "../assets/home.svg";
 
 export default function Home({socket}) {
 	const navigate = useNavigate();
@@ -18,21 +18,26 @@ export default function Home({socket}) {
 
 	return (
 		<section className='home__container'>
-			<img src={Img} className='home__img'/>
-			<h2 className='home__header'>Sign in to Open Chat</h2>
-			<form onSubmit={handleSubmit} className='home__form'>
-				<input 
-					type='text' 
-					minLength={6} 
-					name='username' 
-					placeholder='Username'
-					id='username'
-					className='username__input' 
-					value={userName} 
-					onChange={e => setUserName(e.target.value)}
-				/>
-				<Button className='home__cta'>Sign In</Button>
-			</form>
+			<h2 className='home__header'>Welcome back! 👋🏻</h2>
+
+			<div>
+				<img src={Img} className='home__img'/>
+				
+				<form onSubmit={handleSubmit} className='home__form'>
+					<h3 className='home__header'>Login</h3>
+					<input 
+						type='text' 
+						minLength={6} 
+						name='username' 
+						placeholder='Username'
+						id='username'
+						className='username__input' 
+						value={userName} 
+						onChange={e => setUserName(e.target.value)}
+					/>
+					<Button className='home__cta'>Sign In</Button>
+				</form>
+			</div>
 		</section>
 	);
 }
